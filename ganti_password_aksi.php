869e0944c522a9559e0148d78566a885c8d3e879
Sulthan Zahran <ran.sulthan@gmail.com>
2024-05-28 20:25:30 +0700

    Delete ganti_password_aksi.php

--- ganti_password_aksi.php
+++
@@ -1,5 +0,0 @@
-<?php
-include '../koneksi.php';
-$password_baru = md5($_POST['password_baru']);
-mysqli_query($koneksi,"update admin set password='$password_baru'");
-header("location:ganti_password.php?pesan=oke");
